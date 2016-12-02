982dc8cdd202761b72dbceca5cba3945289c0dcd
Maxwell Klein <bklein@vtsmedical.com>
2016-12-02 01:34:16 -0500

    cleanup nested ifs

--- app/src/main/java/fretx/version4/paging/tuner/TunerFragment.java
+++ app/src/main/java/fretx/version4/paging/tuner/TunerFragment.java
@@ -34,13 +34,10 @@ public class TunerFragment extends Fragment {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		if(mActivity != null){
-			if(mActivity.audio != null){
-				mActivity.audio.disablePitchDetector();
-				mActivity.audio.disableNoteDetector();
-				mActivity.audio.disableChordDetector();
-			}
-		}
+		if(mActivity == null || mActivity.audio == null) return;
+		mActivity.audio.disablePitchDetector();
+		mActivity.audio.disableNoteDetector();
+		mActivity.audio.disableChordDetector();
 	}
 
 	@Override
