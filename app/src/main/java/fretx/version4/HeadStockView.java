19e990de03ecc9f775c1702a80ac061d1fc79773
pandorgithub <pandor@protonmail.com>
2017-06-23 00:32:25 +0200

    [+] hammer selection

--- app/src/main/java/fretx/version4/HeadStockView.java
+++ app/src/main/java/fretx/version4/HeadStockView.java
@@ -7,7 +7,9 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -16,6 +18,8 @@ import android.view.View;
  */
 
 public class HeadStockView extends View{
+    private static final int NB_HAMMERS = 6;
+    private static final float HAMMER_RADIUS = 0.05f;
     private static final float H1RX = 0.08f;
     private static final float H1RY = 0.28f;
     private static final float H2RX = 0.08f;
@@ -30,6 +34,7 @@ public class HeadStockView extends View{
     private static final float H6RY = 0.50f;
 
     private int viewHeight, viewWidth;
+    //headstock
     private final Bitmap headStockBitmap;
     private final int headStockImageIntrinsicHeight;
     private final int headStockImageIntrinsicWidth;
@@ -39,6 +44,22 @@ public class HeadStockView extends View{
     private int headStockImageHeight;
     private int headStockImageWidth;
     private final Matrix headStockMatrix = new Matrix();
+    //hammers
+    private class Hammer {
+        float cx = 0;
+        float cy = 0;
+
+        public void set(float cx, float cy) {
+            this.cx = cx;
+            this.cy = cy;
+        }
+    }
+    private final Hammer hammers[] = new Hammer[NB_HAMMERS];
+    private float hammerClickRadius;
+    private int selectedHammerIndex = -1;
+    //listener
+    private OnClickListener listener;
+
 
     private final Paint circlePainter = new Paint();
 
@@ -70,6 +91,38 @@ public class HeadStockView extends View{
         headStockMatrix.reset();
         headStockMatrix.postScale(headStockImageRatio, headStockImageRatio);
         headStockMatrix.postTranslate(headStockImagePosX, headStockImagePosY);
+
+        hammers[0].set(headStockImagePosX + headStockImageWidth * H1RX, headStockImagePosY + headStockImageHeight * H1RY);
+        hammers[1].set(headStockImagePosX + headStockImageWidth * H2RX, headStockImagePosY + headStockImageHeight * H2RY);
+        hammers[2].set(headStockImagePosX + headStockImageWidth * H3RX, headStockImagePosY + headStockImageHeight * H3RY);
+        hammers[3].set(headStockImagePosX + headStockImageWidth * H4RX, headStockImagePosY + headStockImageHeight * H4RY);
+        hammers[4].set(headStockImagePosX + headStockImageWidth * H5RX, headStockImagePosY + headStockImageHeight * H5RY);
+        hammers[5].set(headStockImagePosX + headStockImageWidth * H6RX, headStockImagePosY + headStockImageHeight * H6RY);
+        hammerClickRadius = HAMMER_RADIUS * headStockImageHeight;
+        hammerClickRadius *= hammerClickRadius;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            final float x = event.getX();
+            final float y = event.getY();
+
+            for (int index = 0; index < hammers.length; ++index) {
+                final float dx = hammers[index].cx - x;
+                final float dy = hammers[index].cy - y;
+                if (dx * dx + dy * dy < hammerClickRadius) {
+                    if (selectedHammerIndex != index) {
+                        selectedHammerIndex = index;
+                        if (listener != null) {
+                            listener.notify();
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     @Override
@@ -78,4 +131,11 @@ public class HeadStockView extends View{
         //canvas.drawCircle(headStockImagePosX + headStockImageWidth * H6RX, headStockImagePosY + headStockImageHeight * H6RY, 0.05f * headStockImageHeight, circlePainter);
     }
 
+    public void setOnClickListener(@NonNull OnClickListener listener) {
+        this.listener = listener;
+    }
+
+    public int getSelectedHammerIndex() {
+        return selectedHammerIndex;
+    }
 }
