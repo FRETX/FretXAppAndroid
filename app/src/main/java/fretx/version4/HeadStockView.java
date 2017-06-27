2bc6d480e1732da7b2f811fa4c40ef0764968b50
pandorgithub <pandor@protonmail.com>
2017-06-27 12:58:08 +0200

    [+] electric guitar headstock

--- app/src/main/java/fretx/version4/HeadStockView.java
+++ app/src/main/java/fretx/version4/HeadStockView.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import fretx.version4.utils.HeadstockViewDescriptor;
 import rocks.fretx.audioprocessing.MusicUtils;
 
 /**
@@ -22,74 +23,17 @@ import rocks.fretx.audioprocessing.MusicUtils;
 
 public class HeadStockView extends View{
     private static final String TAG = "KJKP6_HSV";
-    private static final int NB_HAMMERS = 6;
-    //ears
     private static final int TEXT_SIZE = 40;
-    private static final float EAR_RADIUS = 0.05f;
-    private static final float E1RX = 0.09f;
-    private static final float E1RY = 0.51f;
-    private static final float E2RX = 0.09f;
-    private static final float E2RY = 0.36f;
-    private static final float E3RX = 0.09f;
-    private static final float E3RY = 0.215f;
-    private static final float E4RX = 0.88f;
-    private static final float E4RY = 0.215f;
-    private static final float E5RX = 0.88f;
-    private static final float E5RY = 0.36f;
-    private static final float E6RX = 0.88f;
-    private static final float E6RY = 0.51f;
-    //strings
-    private static final float STRING_WIDTH = 0.02f;
-    private static final float S1RX = 0.32f;
-    private static final float S1RY = 0.77f;
-    private static final float S2RX = 0.38f;
-    private static final float S2RY = 0.77f;
-    private static final float S3RX = 0.45f;
-    private static final float S3RY = 0.77f;
-    private static final float S4RX = 0.52f;
-    private static final float S4RY = 0.77f;
-    private static final float S5RX = 0.58f;
-    private static final float S5RY = 0.77f;
-    private static final float S6RX = 0.65f;
-    private static final float S6RY = 0.77f;
     //headstock
+    private HeadstockViewDescriptor descriptor = new HeadstockViewDescriptor(HeadstockViewDescriptor.Headstock.ELECTRIC);
     private final Bitmap headStockBitmap;
     private final int headStockImageIntrinsicHeight;
     private final int headStockImageIntrinsicWidth;
     private final Matrix headStockMatrix = new Matrix();
-    private final Ear ears[] = new Ear[NB_HAMMERS];
-    private float earClickRadius;
-    private float stringWidth;
-    private float stringBottom;
     private int selectedEarIndex;
     private boolean clickable = true;
     private Paint painter = new Paint(); //FOR DEBUG ONLY
-    private class Ear {
-        float erx = 0;
-        float ery = 0;
-        float srx = 0;
-        float sry = 0;
-        float ex = 0;
-        float ey = 0;
-        float sx = 0;
-        float sy = 0;
-        String name;
 
-        Ear(float erx, float ery, float srx, float sry, String name) {
-            this.erx = erx;
-            this.ery = ery;
-            this.srx = srx;
-            this.sry = sry;
-            this.name = name;
-        }
-
-        void update(int x, int y, int px, int py) {
-            ex = px + erx * x;
-            ey = py + ery * y;
-            sx = px + srx * x;
-            sy = py + sry * y;
-        }
-    }
     public interface OnEarSelectedListener {
         void onEarSelected(int selectedIndex);
     }
@@ -98,16 +42,9 @@ public class HeadStockView extends View{
     public HeadStockView(Context context, AttributeSet attrs){
         super(context, attrs);
 
-        headStockBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.classical_headstock);
+        headStockBitmap = BitmapFactory.decodeResource(context.getResources(), descriptor.ressourceId);
         headStockImageIntrinsicHeight = headStockBitmap.getHeight();
         headStockImageIntrinsicWidth = headStockBitmap.getWidth();
-
-        ears[0] = new Ear(E1RX, E1RY, S1RX, S1RY, MusicUtils.midiNoteToName(MusicUtils.getTuningMidiNotes(MusicUtils.TuningName.STANDARD)[0]).substring(0,1));
-        ears[1] = new Ear(E2RX, E2RY, S2RX, S2RY, MusicUtils.midiNoteToName(MusicUtils.getTuningMidiNotes(MusicUtils.TuningName.STANDARD)[1]).substring(0,1));
-        ears[2] = new Ear(E3RX, E3RY, S3RX, S3RY, MusicUtils.midiNoteToName(MusicUtils.getTuningMidiNotes(MusicUtils.TuningName.STANDARD)[2]).substring(0,1));
-        ears[3] = new Ear(E4RX, E4RY, S4RX, S4RY, MusicUtils.midiNoteToName(MusicUtils.getTuningMidiNotes(MusicUtils.TuningName.STANDARD)[3]).substring(0,1));
-        ears[4] = new Ear(E5RX, E5RY, S5RX, S5RY, MusicUtils.midiNoteToName(MusicUtils.getTuningMidiNotes(MusicUtils.TuningName.STANDARD)[4]).substring(0,1));
-        ears[5] = new Ear(E6RX, E6RY, S6RX, S6RY, MusicUtils.midiNoteToName(MusicUtils.getTuningMidiNotes(MusicUtils.TuningName.STANDARD)[5]).substring(0,1));
 
         painter.setTextSize(TEXT_SIZE);
     }
@@ -132,12 +69,7 @@ public class HeadStockView extends View{
         headStockMatrix.postScale(headStockImageRatio, headStockImageRatio);
         headStockMatrix.postTranslate(headStockImagePosX, headStockImagePosY);
 
-        for (Ear hammer: ears) {
-            hammer.update(headStockImageWidth, headStockImageHeight, headStockImagePosX, headStockImagePosY);
-        }
-        earClickRadius = EAR_RADIUS * headStockImageHeight;
-        stringWidth = STRING_WIDTH * headStockImageWidth;
-        stringBottom = headStockImagePosY + headStockImageHeight;
+        descriptor.update(headStockImageWidth, headStockImageHeight, headStockImagePosX, headStockImagePosY);
     }
 
     @Override
@@ -146,10 +78,10 @@ public class HeadStockView extends View{
             final float x = event.getX();
             final float y = event.getY();
 
-            for (int index = 0; index < ears.length; ++index) {
-                final float dx = ears[index].ex - x;
-                final float dy = ears[index].ey - y;
-                if (dx * dx + dy * dy < earClickRadius * earClickRadius) {
+            for (int index = 0; index < descriptor.ears.length; ++index) {
+                final float dx = descriptor.ears[index].ex - x;
+                final float dy = descriptor.ears[index].ey - y;
+                if (dx * dx + dy * dy < descriptor.earRadius * descriptor.earRadius) {
                     if (selectedEarIndex != index) {
                         selectedEarIndex = index;
                         if (listener != null) {
@@ -166,11 +98,11 @@ public class HeadStockView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawBitmap(headStockBitmap, headStockMatrix, null);
-        for (int index = 0; index < ears.length; ++index) {
-            final Ear ear = ears[index];
+        for (int index = 0; index < descriptor.ears.length; ++index) {
+            final HeadstockViewDescriptor.Ear ear = descriptor.ears[index];
             if (index == selectedEarIndex) {
                 painter.setColor(Color.GREEN);
-                canvas.drawRect(ear.sx, ear.sy, ear.sx + stringWidth, stringBottom, painter);
+                canvas.drawRect(ear.sx, ear.sy, ear.sx + descriptor.stringWidth, descriptor.stringBottom, painter);
             } else {
                 painter.setColor(Color.BLACK);
             }
